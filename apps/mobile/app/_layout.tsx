e5f3b9bab5c92e39284abab5a2f70807e9accdce
kudzimusar <77352591+kudzimusar@users.noreply.github.com>
2026-09-10 16:12:19 +0900

    chore(mobile): remove duplicate gesture handler import

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,4 +1,3 @@
-import 'react-native-gesture-handler'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import * as Notifications from 'expo-notifications'
 import { router, Stack } from 'expo-router'
